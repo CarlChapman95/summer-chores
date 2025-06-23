0618a63ebe0057216bb3e57cd290c1920f409608
CarlChapman95 <crabb.chap@gmail.com>
2025-06-23 18:59:56 -0400

    Set all Timeouts

--- callbackVersion.js
+++ callbackVersion.js
@@ -1,18 +1,33 @@
 function mowYard() {
 
+    setTimeout(() => {
+        console.log(`You mowed the yard.`);
+    }, 2000);
 }
 function weedEat() {
 
+    setTimeout(() => {
+        console.log(`You finished using the weed eater.`);
+    }, 1500);
 }
 function trimHedges() {
 
+    setTimeout(() => {
+        console.log(`You finished trimming the hedges.`);
+    }, 1000);
 }
 function collectWood() {
 
+    setTimeout(() => {
+        console.log(`You finished collecting wood.`);
+    }, 2500);
 }
 function waterGarden() {
 
+    setTimeout(() => {
+        console.log(`You finished watering the garden.`);
+    }, 500);
 }
 function doSummerChores() {
-    
+
 }
