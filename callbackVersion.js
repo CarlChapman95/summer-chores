eb8e3a5a36db9fac147788fceeda12eef3e41616
CarlChapman95 <crabb.chap@gmail.com>
2025-06-23 19:24:22 -0400

    Setup all callback functions with the if statement

--- callbackVersion.js
+++ callbackVersion.js
@@ -2,32 +2,58 @@ function mowYard(name, callback) {
 
     setTimeout(() => {
         console.log(`${name} mowed the yard.`);
+        callback();
     }, 2000);
 }
 function weedEat(name, callback) {
 
     setTimeout(() => {
+        let tiredness = Math.floor(Math.random() * 100) + 1;
+        if(tiredness > 33) {
         console.log(`${name} finished using the weed eater.`);
+        } else {
+            console.log(`${name} fell asleep after mowing the yard.`);
+            callback();
+        }
     }, 1500);
 }
 function trimHedges(name, callback) {
 
     setTimeout(() => {
+        let tiredness = Math.floor(Math.random() * 100) + 1;
+        if(tiredness > 33) {
         console.log(`${name} finished trimming the hedges.`);
+        } else {
+            console.log(`${name} fell asleep after weed eating the yard.`);
+            callback();
+        }
     }, 1000);
 }
 function collectWood(name, callback) {
 
     setTimeout(() => {
+        let tiredness = Math.floor(Math.random() * 100) + 1;
+        if(tiredness > 33) {
         console.log(`${name} finished collecting wood.`);
+        } else {
+            console.log(`${name} fell asleep after trimming the hedges.`);
+            callback();
+        }
     }, 2500);
 }
 function waterGarden(name, callback) {
 
     setTimeout(() => {
+        let tiredness = Math.floor(Math.random() * 100) + 1;
+        if(tiredness > 33) {
         console.log(`${name} finished watering the garden.`);
+        } else {
+            console.log(`${name} fell asleep after collecting wood.`);
+            callback();
+        }
     }, 500);
 }
+
 function doSummerChores(name) {
 
 }
